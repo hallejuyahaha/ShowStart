3d85d7b560e69f42ae24fe6c54084151ca443fdc
hallejuyahaha <357563221@qq.com>
2018-05-08 08:54:44 +0800

    updata

---
+++ ShowStart/test.sql
@@ -0,0 +1,49 @@
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : root
+Source Server Version : 50719
+Source Host           : localhost:3306
+Source Database       : test
+
+Target Server Type    : MYSQL
+Target Server Version : 50719
+File Encoding         : 65001
+
+Date: 2018-05-07 17:22:24
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for cnblognews
+-- ----------------------------
+DROP TABLE IF EXISTS `cnblognews`;
+CREATE TABLE `cnblognews` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `author` varchar(255) DEFAULT NULL,
+  `watch` varchar(255) DEFAULT NULL,
+  `comment` varchar(255) DEFAULT NULL,
+  `favorite` varchar(255) DEFAULT NULL,
+  `content` text,
+  `url` varchar(255) DEFAULT NULL,
+  `title` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=288 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for showstart
+-- ----------------------------
+DROP TABLE IF EXISTS `showstart`;
+CREATE TABLE `showstart` (
+  `id` int(11) unsigned zerofill NOT NULL AUTO_INCREMENT,
+  `showname` varchar(255) DEFAULT '',
+  `actor` varchar(255) DEFAULT '',
+  `price` varchar(255) DEFAULT '',
+  `time` varchar(255) DEFAULT NULL,
+  `place` varchar(255) DEFAULT '',
+  `url` varchar(255) DEFAULT '',
+  `type` varchar(255) DEFAULT '',
+  `StartOrEnd` tinyint(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2229 DEFAULT CHARSET=utf8;
